f2a7d64ca70879470268f24d67d4f8798507c0fb
Abhinav Parasar <abhinavparasar262@gmail.com>
2025-07-15 01:18:43 +0530

    Add files via upload

---
+++ data/subjectList.js
@@ -0,0 +1,15 @@
+[
+  {
+    "subject": "Planning",
+    "materials": [
+      { "title": "Town Planning Notes", "file": "../assets/pdfs/planning/tp_notes.pdf" },
+      { "title": "Urban Form Notes", "file": "../assets/pdfs/planning/urban_form.pdf" }
+    ]
+  },
+  {
+    "subject": "Environment",
+    "materials": [
+      { "title": "Environmental Notes", "file": "../assets/pdfs/environment/env_notes.pdf" }
+    ]
+  }
+]
